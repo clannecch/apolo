157a2f439f3aeda46a9721cdf3ad7c362b3b7941
claudio annecchini <annecchini@gmail.com>
2011-05-17 18:47:20 -0300

    se modifico el modelo wage_remunerative_concept. Se modifico la forma que define los belong_to y se definio una clase all_without_me que trae todos los registros menos el que estamos editando esta clase se agrego al _form.html.erb en el combo de seleccion de concepto asociado.
    Ademas se modifico el config\routes.rb por que faltaba la ruta de  wage_remunerative_concept.(por error se genero en otro directorio)

--- app/models/wage_remunerative_concept.rb
+++ app/models/wage_remunerative_concept.rb
@@ -1,6 +1,12 @@
 class WageRemunerativeConcept < ActiveRecord::Base
-  belongs_to :concepto_asociado_haber
-  belongs_to :concepto_asociado_retencion
-  belongs_to :concepto_asociado_haber_2
-  belongs_to :concepto_asociado_retencion_2
+  [:concepto_asociado_haber,
+  :concepto_asociado_retencion,
+  :concepto_asociado_haber_2,
+  :concepto_asociado_retencion_2
+  ].each {|field| belongs_to field, :class_name => "WageRemunerativeConcept"}
+
+  def all_without_me
+    all - [self]
+    #(new_record?)? all : where("id != #{self.id}")
+  end
 end
