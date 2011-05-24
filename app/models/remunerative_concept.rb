f3a8cb2a0ae119e5f55bf2fde50e3ce01677b83b
claudio annecchini <annecchini@gmail.com>
2011-05-24 19:18:07 -0300

    se modifico el modelo wage_remunerative_concept. Se modifico la forma que define los belong_to y se definio una clase all_without_me que trae todos los registros menos el que estamos editando esta clase se agrego al _form.html.erb en el combo de seleccion de concepto asociado.
    Ademas se modifico el config\routes.rb por que faltaba la ruta de  wage_remunerative_concept.(por error se genero en otro directorio)
    
    Se agrego relacion entre gurpo de honorarios y honorarios.
    Se cambio honorarios para que los honorarios relacionados(campos qe apuntan a la misma tabla) funcionen. (se les puso en el modelo :belongs_to :concepto_asociado_haber, :class_name => "RemunerativeConcept")
    se cambio formulario para que estos datos los puda por bloque y solo si hay algun codigo generado

--- app/models/remunerative_concept.rb
+++ app/models/remunerative_concept.rb
@@ -1,7 +1,55 @@
 class RemunerativeConcept < ActiveRecord::Base
-  belongs_to :concepto_asociado_haber
-  belongs_to :concepto_asociado_retencion
-  belongs_to :concepto_asociado_haber_2
-  belongs_to :concepto_asociado_retencion_2
-  has_many :group_remuneration_relations
+  belongs_to :concepto_asociado_haber, :class_name => "RemunerativeConcept"
+  belongs_to :concepto_asociado_retencion, :class_name => "RetentionConcept"
+  belongs_to :concepto_asociado_haber_2, :class_name => "RemunerativeConcept"
+  belongs_to :concepto_asociado_retencion_2, :class_name => "RetentionConcept"
+  #has_many :group_remuneration_relations
+  has_and_belongs_to_many :group_remunerations
+  validate :cannot_asociate_iqual_concept
+  validates_presence_of :detalle, :codigo, :calculation_type, :valor
+
+  def cannot_asociate_iqual_concept
+    if (self.concepto_asociado_haber_id == self.concepto_asociado_haber_2_id) && !self.concepto_asociado_haber_id.nil?
+      errors.add(:base, 'No puede asociar 2 veces el mismo haber')
+    end
+  end
+
+  def all_without_myself
+    (new_record?) ? RemunerativeConcept.all : (RemunerativeConcept.all - [self])
+  end
 end
+
+
+=begin
+
+belongs_to :author
+
+obj.author
+
+asumo que
+- guardaste en RemunerativeConcept campo author_id el numero de referencia
+- El objeto en cuestion sera Autor.find(el numero de referencia almacenado)
+
+
+belongs_to :author, :class_name => "Person"
+
+obj.author
+
+asumo que
+- guardaste en RemunerativeConcept campo author_id el numero de referencia
+- El objeto en cuestion sera Person.find(el numero de referencia almacenado)
+
+
+
+belongs_to :author, :class_name => "Person", :foreign_key => "REF_PERSONA_ASOCIADA_ID"
+
+obj.author
+
+asumo que
+- guardaste en RemunerativeConcept campo REF_PERSONA_ASOCIADA_ID el numero de referencia
+- El objeto en cuestion sera Person.find(el numero de referencia almacenado)
+
+
+has_many :authors, :class_name => "Person"#, :foreign_key => "remunerative_concept_id"
+
+=end
