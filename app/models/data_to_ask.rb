29597658caf08e982d51cf85dba94bb19d099d43
Luis E. Guardiola <lguardiola@gmail.com>
2011-07-07 15:39:04 -0300

    fix de relacion invalidad sobre empleados

--- app/models/data_to_ask.rb
+++ app/models/data_to_ask.rb
@@ -12,6 +12,5 @@
 #
 
 class DataToAsk < ActiveRecord::Base
-    has_many :employees
-    validates_presence_of		    :detalle,															                          :message => "es un dato requerido"
+	validates_presence_of :detalle, :message => "es un dato requerido"
 end
