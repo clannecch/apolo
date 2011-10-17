e27572ac4aebc52d5804f921e1532c037764e1cc
luis petek <lmpetek@gmail.com>
2011-10-17 15:39:00 -0300

    comenta la tarea de un migrate 20111014226_add_column_email_to_users

--- db/migrate/20111014122652_add_column_email_to_users.rb
+++ db/migrate/20111014122652_add_column_email_to_users.rb
@@ -1,9 +1,9 @@
 class AddColumnEmailToUsers < ActiveRecord::Migration
   def self.up
-    add_column :users, :email, :string
+    #add_column :users, :email, :string
   end
 
   def self.down
-    remove_column :users, :email
+    #remove_column :users, :email
   end
 end
