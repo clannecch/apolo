5e0014af4ea94ebfa19ce660473fa61973005b1f
luis petek <lmpetek@gmail.com>
2011-10-17 16:11:43 -0300

    cambio y mas cambio para email-user

--- db/migrate/20111014122652_add_column_email_to_users.rb
+++ db/migrate/20111014122652_add_column_email_to_users.rb
@@ -1,11 +1,11 @@
 class AddColumnEmailToUsers < ActiveRecord::Migration
   def self.up
-    remove_column :users, :email
+    #remove_column :users, :email
     #add_column :users, :email, :string
   end
 
   def self.down
     #remove_column :users, :email
-    add_column :users, :email, :string
+    #add_column :users, :email, :string
   end
 end
