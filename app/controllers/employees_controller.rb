21cf695f960baf7df1cfb8fecf84d1f94f8d4b58
claudio annecchini <annecchini@gmail.com>
2011-10-24 19:08:15 -0300

    prueba impresion logo

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -122,8 +122,11 @@ class EmployeesController < ApplicationController
     end
 
     pdf.stroke_rectangle [400,710], 100, 100
-
-    foto_principal_id = AssociatedDocumentType.where(:document_type => "F").first.id
+    begin
+      foto_principal_id = AssociatedDocumentType.where(:document_type => "F").first.id
+    rescue
+      foto_principal_id = nil
+    end
     if !foto_principal_id.nil?
       photo = @employee.attachments.unscoped.where(:associated_document_type_id => foto_principal_id).first()
       if photo.adjunto_content_type[0..4] = "image"
