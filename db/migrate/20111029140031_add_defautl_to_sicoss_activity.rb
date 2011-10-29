69acc16aa1e344fc76e98e9713b505e71b00b6e2
claudio annecchini <annecchini@gmail.com>
2011-10-29 12:35:26 -0300

    default faltantes en sicos y typo cta bancaria, defaule en employee funcionando

--- db/migrate/20111029140031_add_defautl_to_sicoss_activity.rb
+++ db/migrate/20111029140031_add_defautl_to_sicoss_activity.rb
@@ -1,11 +1,11 @@
 class AddDefautlToSicossActivity < ActiveRecord::Migration
   def self.up
     add_column :sicoss_activities, :default, :boolean
-    add_column :sicoss_condition, :default, :boolean
+    add_column :sicoss_conditions, :default, :boolean
   end
 
   def self.down
     remove_column :sicoss_activities, :default
-    remove_column :sicoss_condition, :default
+    remove_column :sicoss_conditions, :default
   end
 end
